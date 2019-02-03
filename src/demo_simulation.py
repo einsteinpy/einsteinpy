Fixed scaling in lambda

src/demo_simulation.py
@@ -9,12 +9,12 @@ from einsteinpy import constant
 M = 1.989e30 * u.kg
 pos_vec = np.array([150e9, np.pi/2, 0.])
 real_omega = 29951.68/150e9
-vel_vec = np.array([0, 0., real_omega])
+vel_vec = np.array([0, 0., real_omega/16.5])
 
 
 time = 0 * u.s
 cl = Schwarzschild.from_values(pos_vec, vel_vec, time, M)
-ANS = cl.calculate_trajectory(end_lambda=31558464*constant.c.value, OdeMethodKwargs={'stepsize':constant.c.value*3000})
+ANS = cl.calculate_trajectory(end_lambda=10558464, OdeMethodKwargs={'stepsize':80})
 # ANS = cl.calculate_trajectory(end_lambda=300000, OdeMethodKwargs={'vectorized':True})
 ans = ANS[1]
 print('calculate done')

src/einsteinpy/metric/schwarzschild.py
@@ -130,7 +130,7 @@ class Schwarzschild:
         f_vec_vals = np.zeros(shape=vec.shape, dtype=vec.dtype)
         for i in range(len(vec)):
             f_vec_vals[i] = self.f(i,vec)
-        return f_vec_vals
+        return (_c * f_vec_vals)
 
     def calculate_trajectory(self, start_lambda=0.0, end_lambda=1e7, stop_on_singularity=True, OdeMethodKwargs={}):
         """
@@ -139,9 +139,9 @@ class Schwarzschild:
         Parameters
         ----------
         start_lambda : float
-            Starting lambda, defaults to 0.0, ( c*lambda ~= t)
+            Starting lambda, defaults to 0.0, ( c ~= t)
         end_lamdba : float
-            Lambda where iteartions will stop, defaults to 10000000
+            Lambda where iteartions will stop, defaults to 100000
         stop_on_singularity : bool
             Whether to stop further computation on reaching singularity, defaults to True
 