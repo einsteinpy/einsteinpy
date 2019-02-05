0e9ada22065db03b97ec2aa5237345f284c28a14
Shreyas Bapat <bapat.shreyas@gmail.com>
2019-02-05 23:59:19 +0530

    Manage merge conflicts

--- src/einsteinpy/plotting/geodesics.py
+++ src/einsteinpy/plotting/geodesics.py
@@ -1,5 +1,8 @@
-import matplotlib.pyplot as plt
+import random
+
 import astropy.units as u
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 
 from einsteinpy.metric import Schwarzschild
@@ -36,3 +39,70 @@ class StaticGeodesicPlotter:
 
     def show(self):
         plt.show()
+
+
+class StaticGeodesicPlotter:
+    """
+    Class for plotting static matplotlib plots
+    """
+
+    def __init__(self, mass, time=0, ax=None):
+        self.ax = ax
+        if not self.ax:
+            _, self.ax = plt.subplots(figsize=(6, 6))
+        self.time = time
+        self.mass = mass
+        self._attractor_present = False
+
+    def plot_trajectory(self, pos_vec, vel_vec, end_lambda, step_size, color):
+        swc = Schwarzschild.from_spherical(pos_vec, vel_vec, self.time, self.mass)
+
+        vals = swc.calculate_trajectory(
+            end_lambda=end_lambda, OdeMethodKwargs={"stepsize": step_size}
+        )[1]
+
+        time = np.array([coord[0] for coord in vals])
+        r = np.array([coord[1] for coord in vals])
+        theta = np.array([coord[2] for coord in vals])
+        phi = np.array([coord[3] for coord in vals])
+
+        x = r * np.cos(phi)
+        y = r * np.sin(phi)
+
+        lines = self.ax.plot(x, y, "--", color=color)
+
+        return lines, x[-1], y[-1]
+
+    def plot_attractor(self):
+        if not self._attractor_present:
+            self._draw_attractor()
+
+    def _draw_attractor(self, min_radius=0 * u.km):
+        radius = max(schwarzschild_radius(self.mass * u.km), min_radius.to(u.km))
+        color = "#ffcc00"
+
+        self.ax.add_patch(mpl.patches.Circle((0, 0), radius.value, lw=0, color=color))
+
+    def plot(
+        self,
+        pos_vec,
+        vel_vec,
+        end_lambda=10,
+        step_size=1e-3,
+        color="#{:06x}".format(random.randint(0, 0xFFFFFF)),
+    ):
+
+        self.plot_attractor()
+
+        lines, x0, y0 = self.plot_trajectory(
+            pos_vec, vel_vec, end_lambda, step_size, color
+        )
+
+        l, = self.ax.plot(x0, y0, "o", mew=0, color=lines[0].get_color())
+        lines.append(l)
+
+        self.ax.set_xlabel("$x$ (km)")
+        self.ax.set_ylabel("$y$ (km)")
+        self.ax.set_aspect(1)
+
+        return lines
