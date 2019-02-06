ba769db3c8ad7df1c2fae7b374a0163e1e0d9996
Bhavya Bhatt <bhavyabhatt17@gmail.com>
2019-02-06 22:54:11 +0530

    tests for schwarzschild metric

---
+++ src/einsteinpy/tests/tests_metric/test_schwarzschild.py
@@ -0,0 +1,33 @@
+import pytest
+from astropy import units as u
+from astropy.tests.helper import assert_quantity_allclose
+
+from einsteinpy.metric import schwarzschild
+
+def test_constructor_values():
+    pos_vec = # in astropy units
+    vel_vec = 
+    time = 
+    M = 
+    metric_obj = Schwarzschild(pos_vec, vel_vec, time, M)
+    assert metric_obj.pos_vec == pos_vec
+    assert metric_obj.vel_vec == vel_vec
+    assert metric_obj.time == time
+    assert metric_obj.M == M
+
+def test_christoffel_symbols_values():
+
+
+def test_f_values():
+
+
+def test_f_vec_values():
+
+
+def test_calculate_trajectory_four_velocity_constant():
+
+
+def test_calculate_trajectory_vec_values():
+
+
+
