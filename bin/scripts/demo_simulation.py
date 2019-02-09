4ddbbe4ca51ad246990fbaf177e2837c01b3051c
Ritwik Saha <s.ritwik98@gmail.com>
2019-02-09 13:12:59 +0000

    Black approved now!

--- bin/scripts/demo_simulation.py
+++ bin/scripts/demo_simulation.py
@@ -7,17 +7,17 @@ from einsteinpy import constant
 
 
 M = 1.989e30 * u.kg
-pos_vec = np.array([150e9, np.pi/2, 0.])
-real_omega = 29951.68/150e9
-vel_vec = np.array([0, 0., real_omega/15.5])
+pos_vec = np.array([150e9, np.pi / 2, 0.0])
+real_omega = 29951.68 / 150e9
+vel_vec = np.array([0, 0.0, real_omega / 15.5])
 
 
 time = 0 * u.s
 cl = Schwarzschild.from_values(pos_vec, vel_vec, time, M)
-ANS = cl.calculate_trajectory(end_lambda=10558464, OdeMethodKwargs={'stepsize':100})
+ANS = cl.calculate_trajectory(end_lambda=10558464, OdeMethodKwargs={"stepsize": 100})
 # ANS = cl.calculate_trajectory(end_lambda=10558464, OdeMethodKwargs={'vectorized':True, 'first_step':100.0})
 ans = ANS[1]
-print('calculate done')
+print("calculate done")
 
 r = np.array([t[1] for t in ans])
 print(r[-1])
@@ -25,7 +25,7 @@ print(r[-1])
 phi = np.array([t[3] for t in ans])
 time = np.array([t[0] for t in ans])
 x = r * np.cos(phi)
-y = r*np.sin(phi)
-plt.scatter(x,y, s=1, c=time, cmap='Oranges')
-plt.scatter(0, 0 , color='black')
+y = r * np.sin(phi)
+plt.scatter(x, y, s=1, c=time, cmap="Oranges")
+plt.scatter(0, 0, color="black")
 plt.show()
