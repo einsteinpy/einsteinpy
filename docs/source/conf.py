696d7420eb4c5e3d04b24da00b96522fbc7ae9fe
Ritwik Saha <s.ritwik98@gmail.com>
2019-02-10 09:26:45 +0000

    Merge conflicts resolved

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@ import einsteinpy
 
 project = "EinsteinPy"
 year = datetime.now().year
-copyright = "%d EinsteinPy Development Team" %year
+copyright = "%d EinsteinPy Development Team" % year
 
 version = einsteinpy.__version__
 
@@ -13,29 +13,28 @@ highlight_language = "python"
 pygments_style = "sphinx"
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
-    'astropy': ('http://docs.astropy.org/en/stable/', None),
-    'numpy': ('https://docs.scipy.org/doc/numpy/', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/reference', None),
-    'matplotlib': ('http://matplotlib.org', None)
+    "python": ("https://docs.python.org/3", None),
+    "astropy": ("http://docs.astropy.org/en/stable/", None),
+    "numpy": ("https://docs.scipy.org/doc/numpy/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
+    "matplotlib": ("http://matplotlib.org", None),
 }
 
 html_theme_options = {
-    'logo': 'logo_HD.png',
-    'logo_name': True,
-    'logo_text_align': 'center',
-    'travis_button' : True,
+    "logo": "logo_HD.png",
+    "logo_name": True,
+    "logo_text_align": "center",
+    "travis_button": True,
     # 'codecov_button': True,
-    'description':'General Relativity in Python',
-    'body_text_align': 'left',
-    'github_user': 'einsteinpy',
-    'github_repo': 'einsteinpy',
-    'show_relbars': True,
-    'show_powered_by': False,
-    'page_width': '80%',
-    'github_banner': True,
-    'extra_nav_links' : {'Blog': 'https://einsteinpy.github.io/',
-                        },
+    "description": "General Relativity in Python",
+    "body_text_align": "left",
+    "github_user": "einsteinpy",
+    "github_repo": "einsteinpy",
+    "show_relbars": True,
+    "show_powered_by": False,
+    "page_width": "80%",
+    "github_banner": True,
+    "extra_nav_links": {"Blog": "https://einsteinpy.github.io/"},
 }
 
 add_function_parentheses = True
