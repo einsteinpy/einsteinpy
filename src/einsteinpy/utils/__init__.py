cf62571a7d61082ec368474f7cdfb89c4d51e350
Ritwik Saha <s.ritwik98@gmail.com>
2019-03-03 19:19:21 +0000

    added basic Boyer-Lindquist template

--- src/einsteinpy/utils/__init__.py
+++ src/einsteinpy/utils/__init__.py
@@ -1,3 +1,4 @@
+from .bl_coord_transforms import *
 from .coord_transforms import *
 from .scalar_factor import scalar_factor, scalar_factor_derivative
 from .schwarzschild_utils import schwarzschild_radius, time_velocity
---
+++ src/einsteinpy/utils/bl_coord_transforms.py
@@ -0,0 +1,18 @@
+import astropy.units as u
+import numpy as np
+
+
+def CartesianToBL_pos(pos_vec, a):
+    pass
+
+
+def CartesianToBL_vel(pos_vec, vel_vec, a):
+    pass
+
+
+def BLToCartesian_pos(pos_vec, a):
+    pass
+
+
+def BLToCartesian_vel(pos_vec, vel_vec, a):
+    pass
