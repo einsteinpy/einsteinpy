a389847951ce432dfcd5269ee60b012c470073c9
Ritwik Saha <s.ritwik98@gmail.com>
2019-03-05 08:56:52 +0000

    added some more BL coord tests, where a!=0

--- src/einsteinpy/tests/test_utils/test_bl_coord_transforms.py
+++ src/einsteinpy/tests/test_utils/test_bl_coord_transforms.py
@@ -65,3 +65,39 @@ def test_CartesianToBL_vel(pos_vec, vel_vec, a, ans_vec):
 def test_BLtoCartesian_vel(pos_vec, vel_vec, a, ans_vec):
     ans = utils.BLToCartesian_vel(pos_vec, vel_vec, a)
     assert_allclose(ans, ans_vec, rtol=0.0, atol=1e-5)
+
+
+@pytest.mark.parametrize(
+    "pos_vec, a",
+    [
+        (np.array([10 / np.sqrt(2), 10 / np.sqrt(2), 0.0]), 0.7),
+        (np.array([-732.0, 456, -90]), 9.0),
+        (np.array([-732.0, -1456, 90]), 21),
+        (np.array([-10.0, -10, -20]), 0),
+    ],
+)
+def test_cycle_pos(pos_vec, a):
+    pos_vec2 = utils.CartesianToBL_pos(pos_vec, a)
+    pos_vec3 = utils.BLToCartesian_pos(pos_vec2, a)
+    assert_allclose(pos_vec, pos_vec3, rtol=0.0, atol=1e-5)
+
+
+@pytest.mark.parametrize(
+    "pos_vec, vel_vec, a",
+    [
+        (
+            np.array([10 / np.sqrt(2), 10 / np.sqrt(2), 0.0]),
+            np.array([100, -100, -43]),
+            0.7,
+        ),
+        (np.array([-732.0, 456, -90]), np.array([2, 2, 2]), 9.0),
+        (np.array([-732.0, -1456, 90]), np.array([3.4, -100, -43]), 21),
+        (np.array([-10.0, -10, -20]), np.array([100, 100, 43]), 0),
+    ],
+)
+def test_cycle_vel(pos_vec, vel_vec, a):
+    pos_vec2 = utils.CartesianToBL_pos(pos_vec, a)
+    vel_vec2 = utils.CartesianToBL_vel(pos_vec, vel_vec, a)
+    # pos_vec3 = utils.BLToCartesian_pos(pos_vec2, a)
+    vel_vec3 = utils.BLToCartesian_vel(pos_vec2, vel_vec2, a)
+    assert_allclose(vel_vec, vel_vec3, rtol=0.0, atol=1e-5)
