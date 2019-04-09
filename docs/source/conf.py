d5e1e54cd59599c067a0c1f723462051f7276e6f
divya144 <b17083@students.iitmandi.ac.in>
2019-04-09 19:53:09 +0530

    Mobile compatibility of Documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -70,6 +70,14 @@ html_theme_path = [alabaster.get_path()]
 
 html_title = "EinsteinPy"
 
+html_static_path =['_static']
+
+html_context = {
+    'css_files': [
+        '_static/override.css',
+        ],
+    }
+
 htmlhelp_basename = 'einsteinpydoc'
 html_sidebars = {
     "**": [
