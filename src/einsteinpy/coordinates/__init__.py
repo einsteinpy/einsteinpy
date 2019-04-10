6c4667c2dab9d2c8d65b4723f70622f90cdf19e1
rishi-s8 <33762894+rishi-s8@users.noreply.github.com>
2019-04-10 19:57:12 +0530

    Add Differential Classes to __init__.py

--- src/einsteinpy/coordinates/__init__.py
+++ src/einsteinpy/coordinates/__init__.py
@@ -1,1 +1,2 @@
 from einsteinpy.coordinates.core import BoyerLindquist, Cartesian, Spherical
+from einsteinpy.coordinates.velocity import BoyerLindquistDifferential, CartesianDifferential, SphericalDifferential
