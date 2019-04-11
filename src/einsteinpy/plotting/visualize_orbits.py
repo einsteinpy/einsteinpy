1ff3ed838013b53f467128f62d1d9d240ef46a15
Manvi07 <b17092@students.iitmandi.ac.in>
2019-04-11 20:29:42 +0530

    reformatted 1 file

--- src/einsteinpy/plotting/visualize_orbits.py
+++ src/einsteinpy/plotting/visualize_orbits.py
@@ -15,25 +15,27 @@ class VisualizeOrbits:
         self.epochs = epochs
 
     def create_object(self):
-        obj = Horizons(id = self.Id, location = self.location, epochs = self.epochs, id_type='id')
+        obj = Horizons(
+            id=self.Id, location=self.location, epochs=self.epochs, id_type="id"
+        )
         return obj
 
     def velocity(self):
-        #Calculate velocity of the object with respect to sun and the observer.
-        #UNITS: Km/s and Km/s
-        print(self.create_object().ephemerides(quantities= 22))
+        # Calculate velocity of the object with respect to sun and the observer.
+        # UNITS: Km/s and Km/s
+        print(self.create_object().ephemerides(quantities=22))
 
     def S_distance(self):
-        #Calculates "Sub solar" point positional angle and the angular distance from the sub-observer point (center of disk).
-        #UNITS: Degrees and Arcseconds
-        print(self.create_object().ephemerides(quantities= 16))
+        # Calculates "Sub solar" point positional angle and the angular distance from the sub-observer point (center of disk).
+        # UNITS: Degrees and Arcseconds
+        print(self.create_object().ephemerides(quantities=16))
 
     def N_distance(self):
-        #Calculates North Pole positional angle and the angular distance from the sub-observer point (center of disk).
-        #UNITS: Degrees and Arcseconds
-        print(self.create_object().ephemerides(quantities= 17))
+        # Calculates North Pole positional angle and the angular distance from the sub-observer point (center of disk).
+        # UNITS: Degrees and Arcseconds
+        print(self.create_object().ephemerides(quantities=17))
 
     def az_el(self):
-        #The apparent azimuthal and elevation angle of the target. Elevation is taken perpendicular to the local zentih direction.
-        #UNITS: Degrees
-        print(self.create_object().ephemerides(quantities= 4))
+        # The apparent azimuthal and elevation angle of the target. Elevation is taken perpendicular to the local zentih direction.
+        # UNITS: Degrees
+        print(self.create_object().ephemerides(quantities=4))
