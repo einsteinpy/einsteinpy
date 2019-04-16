0d392d5c3fe9d166ee6bfb528e3fd4d4674e1de2
Tushar-Tyagi <b17106@students.iitmandi.ac.in>
2019-04-17 00:52:53 +0530

    isort fix

--- src/einsteinpy/utils/__init__.py
+++ src/einsteinpy/utils/__init__.py
@@ -1,5 +1,5 @@
 from .bl_coord_transforms import *
 from .coord_transforms import *
+from .jplephem import *
 from .scalar_factor import scalar_factor, scalar_factor_derivative
 from .schwarzschild_utils import schwarzschild_radius, time_velocity
-from .jplephem import *
