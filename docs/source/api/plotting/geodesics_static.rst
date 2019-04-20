c2f284f61de4034d6b3e5cb0ca5295414df5453f
rtanmay <rtanmay05@gmail.com>
2019-04-21 02:49:36 +0530

    Add Color, Auto and Manual scaling for attractor radius.
    This commit will add the Color options for attractor and geodesic as well Automatic and Manual scaling for attractor radius to make plots look better since the schwarzchild radius can be really small and won't be visible.

--- docs/source/api/plotting/geodesics_static.rst
+++ docs/source/api/plotting/geodesics_static.rst
@@ -6,3 +6,60 @@ This module contains the basic classes for static plottings in
 
 .. automodule:: einsteinpy.plotting.geodesics_static
     :members:
+
+Auto and Manual scaling
+=======================
+EinsteinPy supports Automatic and Manual scaling of the attractor to make plots look better since radius of 
+attractor can be really small and not visible.
+
+* Manual_Scaling : 
+    If the user provides the attractor_radius_scale, then the autoscaling will not work.
+    This is checked by initialising the  attractor_radius_scale by -1 and if the user enters the value then 
+    it will be >0 so the value won't remain -1 which is easily checked.
+    
+    The radius is multiplied to the value given in attractor_radius_scale
+    
+    .. code-block:: python
+
+        radius = radius * self.attractor_radius_scale
+
+* Auto Scaling : 
+    If the user does not provide the attractor_radius_scale, the value will be initialised to -1 and 
+    then we will call the auto scaling function. In autoscaling, the attractor radius is first 
+    initialised to the minimum distance between the attractor and the object moving around it. 
+    Now, if this radius is greater than the 1/12th of minimum of range of X and Y coordinates then,
+    the radius is initialised to this minimum. This is done so that the plots are easy to look at.
+
+    minrad_nooverlap : Stores the minimum distance between the particle and attractor
+    .. code-block:: python
+
+        for i in range(0, len(self.xarr)):
+            minrad_nooverlap = min( minrad_nooverlap, self.mindist(self.xarr[i], self.yarr[i]))
+
+    minlen_plot : Stores the minimum of range of X and Y axis
+    
+    .. code-block:: python
+            
+            xlen = max(self.xarr) - min(self.xarr)
+            ylen = max(self.yarr) - min(self.yarr)
+            minlen_plot = min(xlen, ylen)
+    
+    multiplier : Stores the value which is multiplied to the radius to make it 1/12th of the minlenplot
+    
+    .. code-block:: python
+            
+            mulitplier = minlen_plot / (12 * radius)
+            min_radius = radius * mulitplier
+
+
+Attractor Color
+===============
+* Color Options : 
+    User can give the color to attractor of his/her choice.
+    It can be passed while calling the geodesics_static class.
+    Default color of attractor is "black".
+
+    .. code-block:: python
+        
+        self.attractor_color = attractor_color
+        mpl.patches.Circle( (0, 0), radius.value, lw=0, color=self.attractor_color)
--- src/einsteinpy/plotting/geodesics_static.py
+++ src/einsteinpy/plotting/geodesics_static.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 import astropy.units as u
 import matplotlib as mpl
@@ -11,18 +12,56 @@ from einsteinpy.utils import schwarzschild_radius
 
 class StaticGeodesicPlotter:
     """
-    Class for plotting static matplotlib plots
+    Class for plotting static matplotlib plots.
     """
 
-    def __init__(self, mass, time=0 * u.s, ax=None):
+    def __init__(
+        self,
+        mass,
+        time=0 * u.s,
+        ax=None,
+        attractor_radius_scale=-1.0,
+        attractor_color="#ffcc00",
+    ):
+        """
+
+        Parameters
+        ----------
+        attractor_radius_scale : float, optional
+            Scales the attractor radius by the value given. Default is 1. It is used to make plots look more clear if needed.
+        attractor_color : string, optional
+            Color which is used to denote the attractor. Defaults to #ffcc00.
+
+        """
         self.ax = ax
         if not self.ax:
             _, self.ax = plt.subplots(figsize=(6, 6))
         self.time = time
         self.mass = mass
         self._attractor_present = False
+        self.attractor_color = attractor_color
+        self.attractor_radius_scale = attractor_radius_scale
+        self.__xarr = np.array([])
+        self.__yarr = np.array([])
+        self.get_curr_plot_radius = 0
 
     def plot_trajectory(self, pos_vec, vel_vec, end_lambda, step_size, color):
+        """
+
+        Parameters
+        ----------
+        pos_vec : list
+            list of r, theta & phi components along with ~astropy.units.
+        vel_vec : list
+            list of velocities of r, theta & phi components along with ~astropy.units.
+        end_lambda : float, optional
+            Lambda where iteartions will stop.
+        step_size : float, optional
+            Step size for the ODE.
+        color : string
+            Color of the Geodesic
+
+        """
         swc = Schwarzschild.from_spherical(pos_vec, vel_vec, self.time, self.mass)
 
         vals = swc.calculate_trajectory(
@@ -45,10 +84,69 @@ class StaticGeodesicPlotter:
         if not self._attractor_present:
             self._draw_attractor()
 
-    def _draw_attractor(self, min_radius=10 * u.km):
-        radius = max((schwarzschild_radius(self.mass) * 10000), min_radius.to(u.km))
-        color = "#ffcc00"
-        self.ax.add_patch(mpl.patches.Circle((0, 0), radius.value, lw=0, color=color))
+    def __get_x_y(self, pos_vec, vel_vec, end_lambda, step_size):
+        """
+
+        Parameters
+        ----------
+        pos_vec : list
+            list of r, theta & phi components along with ~astropy.units.
+        vel_vec : list
+            list of velocities of r, theta & phi components along with ~astropy.units.
+        end_lambda : float, optional
+            Lambda where iteartions will stop.
+        step_size : float, optional
+            Step size for the ODE.
+
+        """
+        swc = Schwarzschild.from_spherical(pos_vec, vel_vec, self.time, self.mass)
+
+        vals = swc.calculate_trajectory(
+            end_lambda=end_lambda, OdeMethodKwargs={"stepsize": step_size}
+        )[1]
+
+        time = np.array([coord[0] for coord in vals])
+        r = np.array([coord[1] for coord in vals])
+        theta = np.array([coord[2] for coord in vals])
+        phi = np.array([coord[3] for coord in vals])
+
+        x = r * np.cos(phi)
+        y = r * np.sin(phi)
+        self.__xarr = x
+        self.__yarr = y
+        return x, y
+
+    def mindist(self, x, y):
+        return np.sqrt(x * x + y * y)
+
+    def _draw_attractor(self):
+        radius = schwarzschild_radius(self.mass)
+        if self.attractor_radius_scale == -1.0:
+            minrad_nooverlap = self.mindist(self.__xarr[0], self.__yarr[0])
+            for i in range(0, len(self.__xarr)):
+                minrad_nooverlap = min(
+                    minrad_nooverlap, self.mindist(self.__xarr[i], self.__yarr[i])
+                )
+
+            xlen = max(self.__xarr) - min(self.__xarr)
+            ylen = max(self.__yarr) - min(self.__yarr)
+            minlen_plot = min(xlen, ylen)
+            mulitplier = minlen_plot / (12 * radius)
+            min_radius = radius * mulitplier
+
+            radius = min(min_radius, minrad_nooverlap)
+            self.get_curr_plot_radius = radius
+            self.ax.add_patch(
+                mpl.patches.Circle((0, 0), radius, lw=0, color=self.attractor_color)
+            )
+        else:
+            radius = radius * self.attractor_radius_scale
+            self.get_curr_plot_radius = radius
+            self.ax.add_patch(
+                mpl.patches.Circle(
+                    (0, 0), radius.value, lw=0, color=self.attractor_color
+                )
+            )
 
     def plot(
         self,
@@ -58,7 +156,25 @@ class StaticGeodesicPlotter:
         step_size=1e-3,
         color="#{:06x}".format(random.randint(0, 0xFFFFFF)),
     ):
+        """
 
+        Parameters
+        ----------
+        pos_vec : list
+            list of r, theta & phi components along with ~astropy.units.
+        vel_vec : list
+            list of velocities of r, theta & phi components along with ~astropy.units.
+        end_lambda : float, optional
+            Lambda where iteartions will stop.
+        step_size : float, optional
+            Step size for the ODE.
+        color : string
+            Color of the Geodesic
+
+        """
+        self.__xarr, self.__yarr = self.__get_x_y(
+            pos_vec, vel_vec, end_lambda, step_size
+        )
         self.plot_attractor()
         self._attractor_present = True
 
@@ -77,3 +193,6 @@ class StaticGeodesicPlotter:
 
     def save(self, name="static_geodesic.png"):
         plt.savefig(name)
+
+    def show(self):
+        plt.show()
