a25fd684ff936f9760fa361ce85f18d396d3586f
Shreyas Bapat <bapat.shreyas@gmail.com>
2019-04-28 01:15:46 +0530

    Major updations in setup.py - Packaging fixes

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
-import os
 
 from setuptools import setup
 
 # https://packaging.python.org/guides/single-sourcing-package-version/
-version = {}
-with open(os.path.join("src", "einsteinpy", "__init__.py")) as fp:
-    exec(fp.read(), version)
-
-
 # http://blog.ionelmc.ro/2014/05/25/python-packaging/
-setup(version=version["__version__"])
+
+setup(setup_cfg=True)
