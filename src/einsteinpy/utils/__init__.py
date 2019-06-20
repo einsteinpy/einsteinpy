5c81218a669865fceae1c2bed42589c437ffac6a
sofia_ov <ortinvela.sofia@gmail.com>
2019-06-20 21:34:26 +0200

    Add imports of utils __init__

---
+++ src/einsteinpy/utils/__init__.py
@@ -0,0 +1,6 @@
+from .scalar_factor import scalar_factor, scalar_factor_derivative
+from .schwarzschild_utils import (
+    schwarzschild_radius,
+    schwarzschild_radius_dimensionless,
+    time_velocity,
+)
