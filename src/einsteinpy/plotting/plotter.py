933902320cc12e1d83ba87b3247f00762d0486ec
varunvaruns9 <varunsinghs2021@gmail.com>
2019-07-03 19:55:50 +0530

    Refactor: Automatically switch between base classes.
    Remove the duplicate code.

--- src/einsteinpy/plotting/plotter.py
+++ src/einsteinpy/plotting/plotter.py
@@ -10,61 +10,15 @@ def in_ipynb():
     try:
         shell = get_ipython().__class__.__name__  # type: ignore
         if shell == "ZMQInteractiveShell":
-            return True
+            return PlotlyPlotter
         if shell == "TerminalInteractiveShell":
-            return False
-        return False
+            return MatplotlibPlotter
+        return MatplotlibPlotter
     except NameError:
-        return False
+        return MatplotlibPlotter
 
 
-class Plotter(MatplotlibPlotter, PlotlyPlotter):
+class Plotter(in_ipynb()):  # type: ignore
     """
     Class for automatically switching between Matplotlib and Plotly depending on platform used.
     """
-
-    def __init__(self, attractor_color="#ffcc00"):
-        """
-        Constructor.
-
-        Parameters
-        ----------
-        attractor_color : string, optional
-            Color which is used to denote the attractor. Defaults to #ffcc00.
-
-        """
-        if in_ipynb():
-            PlotlyPlotter.__init__(self, attractor_color=attractor_color)
-            self._notebook = True
-            init_notebook_mode(connected=True)
-        else:
-            MatplotlibPlotter.__init__(self, attractor_color=attractor_color)
-            self._notebook = False
-
-    def plot(self, geodesic, color="#{:06x}".format(random.randint(0, 0xFFFFFF))):
-        """
-
-        Parameters
-        ----------
-        geodesic : ~einsteinpy.geodesic.Geodesic
-            Geodesic of the body
-        color : hex code RGB, optional
-            Color of the dashed lines. Picks a random color by default.
-
-        """
-        if self._notebook:
-            PlotlyPlotter.plot(self, geodesic, color=color)
-        else:
-            MatplotlibPlotter.plot(self, geodesic, color=color)
-
-    def show(self):
-        if self._notebook:
-            return PlotlyPlotter.show(self)
-        else:
-            MatplotlibPlotter.show(self)
-
-    def save(self, name):
-        if self._notebook:
-            PlotlyPlotter.save(self, name)
-        else:
-            MatplotlibPlotter.save(self, name)
