6805ed5583b12e10e6a1aa14785a996961e84b10
sofia_ov <ortinvela.sofia@gmail.com>
2019-07-10 11:13:29 +0200

    Fix __init__.py

--- src/einsteinpy/symbolic/__init__.py
+++ src/einsteinpy/symbolic/__init__.py
@@ -1,7 +1,7 @@
 from .christoffel import ChristoffelSymbols
 from .einstein import EinsteinTensor
 from .metric import MetricTensor
-from .ricci import RicciTensor
+from .ricci import RicciScalar, RicciTensor
 from .riemann import RiemannCurvatureTensor
 from .stress_energy_momentum import StressEnergyMomentumTensor
 from .tensor import Tensor
