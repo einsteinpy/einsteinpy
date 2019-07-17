3583109dd8c4915f8fbf10d7cfdde30f1d063163
sofia_ov <ortinvela.sofia@gmail.com>
2019-07-17 15:27:17 +0200

    Fix __init__.py for WeylTensor

--- src/einsteinpy/symbolic/__init__.py
+++ src/einsteinpy/symbolic/__init__.py
@@ -6,3 +6,4 @@ from .riemann import RiemannCurvatureTensor
 from .stress_energy_momentum import StressEnergyMomentumTensor
 from .tensor import Tensor
 from .vacuum_metrics import SchwarzschildMetric
+from .weyl import WeylTensor
