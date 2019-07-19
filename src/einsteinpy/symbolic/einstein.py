28230333fd82ef738b70881b1b9b164a05f82d7c
sofia_ov <ortinvela.sofia@gmail.com>
2019-07-19 10:51:15 +0200

    Einstein Tensor

--- src/einsteinpy/symbolic/einstein.py
+++ src/einsteinpy/symbolic/einstein.py
@@ -1,5 +1,112 @@
-from .tensor import Tensor
+import sympy
+
+from einsteinpy.symbolic.ricci import RicciScalar, RicciTensor
+from einsteinpy.symbolic.tensor import Tensor, _change_config
 
 
 class EinsteinTensor(Tensor):
-    pass
+    """
+    Class for defining Einstein Tensor
+    """
+
+    def __init__(self, arr, syms, config="ll", parent_metric=None):
+        """
+        Constructor and Initializer
+
+        Parameters
+        ----------
+        arr : ~sympy.tensor.array.dense_ndim_array.ImmutableDenseNDimArray or list
+            Sympy Array or multi-dimensional list containing Sympy Expressions
+        syms : tuple or list
+            Tuple of crucial symbols denoting time-axis, 1st, 2nd, and 3rd axis (t,x1,x2,x3)
+        config : str
+            Configuration of contravariant and covariant indices in tensor. 'u' for upper and 'l' for lower indices. Defaults to 'll'.
+        parent_metric : ~einsteinpy.symbolic.metric.MetricTensor or None
+            Corresponding Metric for the Einstein Tensor.
+            Defaults to None.
+
+        Raises
+        ------
+        TypeError
+            Raised when arr is not a list or sympy Array
+        TypeError
+            syms is not a list or tuple
+        ValueError
+            config has more or less than 2 indices
+
+        """
+        super(EinsteinTensor, self).__init__(arr, config=config)
+        self._order = 2
+        self._parent_metric = parent_metric
+        if isinstance(syms, (list, tuple)):
+            self.syms = syms
+            self.dims = len(self.syms)
+        else:
+            raise TypeError("syms should be a list or tuple")
+        if not len(config) == self._order:
+            raise ValueError("config should be of length {}".format(self._order))
+
+    @property
+    def parent_metric(self):
+        """
+        Returns the Parent Metric, if available.
+        """
+        return self._parent_metric
+
+    @classmethod
+    def from_metric(cls, metric):
+        t_ricci = RicciTensor.from_metric(metric)
+        r_scalar = RicciScalar.from_riccitensor(t_ricci, t_ricci.parent_metric)
+        einstein_tensor = t_ricci.tensor() - 0.5 * metric.tensor() * r_scalar.tensor()
+        return cls(
+            einstein_tensor,
+            metric.syms,
+            config="ll",
+            parent_metric=t_ricci.parent_metric,
+        )
+
+    def change_config(self, newconfig="ul", metric=None):
+        """
+        Changes the index configuration(contravariant/covariant)
+
+        Parameters
+        ----------
+        newconfig : str
+            Specify the new configuration. Defaults to 'ul'
+        metric : ~einsteinpy.symbolic.metric.MetricTensor or None
+            Parent metric tensor for changing indices.
+            Already assumes the value of the metric tensor from which it was initialized if passed with None.
+            Compulsory if somehow does not have a parent metric. Defaults to None.
+
+        Returns
+        -------
+        ~einsteinpy.symbolic.einstein.EinsteinTensor
+            New tensor with new configuration. Defaults to 'ul'
+
+        Raises
+        ------
+        Exception
+            Raised when a parent metric could not be found.
+
+        """
+        if metric is None:
+            metric = self._parent_metric
+        if metric is None:
+            raise Exception("Parent Metric not found, can't do configuration change")
+        new_tensor = _change_config(self, metric, newconfig)
+        new_obj = RicciTensor(
+            new_tensor, self.syms, config=newconfig, parent_metric=metric
+        )
+        return new_obj
+
+    def symbols(self):
+        """
+        Returns the symbols used for defining the time & spacial axis
+
+        Returns
+        -------
+        tuple
+            tuple containing (t,x1,x2,x3)
+
+        """
+        return self.syms
