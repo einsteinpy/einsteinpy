e8b14c89dc680999353eef8e52c66f86bcb8d28f
Ritwik Saha <s.ritwik98@gmail.com>
2019-07-30 00:00:15 +0530

    BaseRelativityTensor

--- src/einsteinpy/symbolic/__init__.py
+++ src/einsteinpy/symbolic/__init__.py
@@ -4,6 +4,6 @@ from .metric import MetricTensor
 from .ricci import RicciScalar, RicciTensor
 from .riemann import RiemannCurvatureTensor
 from .stress_energy_momentum import StressEnergyMomentumTensor
-from .tensor import Tensor
+from .tensor import BaseRelativityTensor, Tensor
 from .vacuum_metrics import SchwarzschildMetric
 from .weyl import WeylTensor
