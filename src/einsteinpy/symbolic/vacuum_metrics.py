44a57f0c1ef8020faa5ac235d206626574bdba49
sofia_ov <ortinvela.sofia@gmail.com>
2019-08-17 01:09:19 +0200

    C

--- src/einsteinpy/symbolic/vacuum_metrics.py
+++ src/einsteinpy/symbolic/vacuum_metrics.py
@@ -26,3 +26,27 @@ def SchwarzschildMetric(symbolstr="t r theta phi"):
     list2d[3][3] = -1 * (syms[1] ** 2) * (sympy.sin(syms[2]) ** 2) / (c ** 2)
 
     return MetricTensor(list2d, syms)
+
+def KerrMetric(symbolstr="t r theta phi"):
+    """
+    Returns  Kerr Tensor  of symbols of Schwarzschild Metric.
+
+    Parameters
+    ----------
+    symbolstr : string
+        symbols to be used to define kerr space, defaults to 't r theta phi'
+
+    Returns
+    -------
+    ~einsteinpy.symbolic.metric.MetricTensor
+        Metric Tensor for Kerr space-time
+    """
+    list2d = [[0 for i in range(4)] for i in range(4)]
+    syms = sympy.symbols(symbolstr)
+    c, a = sympy.symbols("c a")
+    list2d[0][0] = 1 - (a / syms[1])
+    list2d[1][1] = -1 / ((1 - (a / syms[1])) * (c ** 2))
+    list2d[2][2] = -1 * (syms[1] ** 2) / (c ** 2)
+    list2d[3][3] = -1 * (syms[1] ** 2) * (sympy.sin(syms[2]) ** 2) / (c ** 2)
+
+    return MetricTensor(list2d, syms)
