b0f39c53cab6be7e537d79cc84bc3d1f477e8214
Shreyas Bapat <bapat.shreyas@gmail.com>
2019-08-27 22:48:00 +0530

    njit all the functions in hope of getting faster outputs

--- src/einsteinpy/coordinates/conversion.py
+++ src/einsteinpy/coordinates/conversion.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+from .utils import (
+    bl_to_cartesian_fast,
+    cartesian_to_bl_fast,
+    cartesian_to_spherical_fast,
+    spherical_to_cartesian_fast,
+)
+
 
 class CartesianConversion:
     def __init__(self, x, y, z, v_x=None, v_y=None, v_z=None):
@@ -19,49 +26,27 @@ class CartesianConversion:
         return self.x_si, self.y_si, self.z_si
 
     def convert_spherical(self):
-        r = np.sqrt(self.x_si ** 2 + self.y_si ** 2 + self.z_si ** 2)
-        theta = np.arccos(self.z_si / r)
-        phi = np.arctan2(self.y_si, self.x_si)
-        if self._velocities_provided:
-            n1 = self.x_si ** 2 + self.y_si ** 2
-            n2 = n1 + self.z_si ** 2
-            v_r = (
-                self.x_si * self.vx_si + self.y_si * self.vy_si + self.z_si * self.vz_si
-            ) / np.sqrt(n2)
-            v_t = (
-                self.z_si * (self.x_si * self.vx_si + self.y_si * self.vy_si)
-                - n1 * self.vz_si
-            ) / (n2 * np.sqrt(n1))
-            v_p = -1 * (self.vx_si * self.y_si - self.x_si * self.vy_si) / n1
-
-            return r, theta, phi, v_r, v_t, v_p
-        return r, theta, phi
+        return cartesian_to_spherical_fast(
+            self.x_si,
+            self.y_si,
+            self.z_si,
+            self.vx_si,
+            self.vy_si,
+            self.vz_si,
+            self._velocities_provided,
+        )
 
     def convert_bl(self, a):
-        w = (self.x_si ** 2 + self.y_si ** 2 + self.z_si ** 2) - (a ** 2)
-        r = np.sqrt(0.5 * (w + np.sqrt((w ** 2) + (4 * (a ** 2) * (self.z_si ** 2)))))
-        theta = np.arccos(self.z_si / r)
-        phi = np.arctan2(self.y_si, self.x_si)
-        if self._velocities_provided:
-            dw_dt = 2 * (
-                self.x_si * self.vx_si + self.y_si * self.vy_si + self.z_si * self.vz_si
-            )
-            v_r = (1 / (2 * r)) * (
-                (dw_dt / 2)
-                + (
-                    (w * dw_dt + 4 * (a ** 2) * self.z_si * self.vz_si)
-                    / (2 * np.sqrt((w ** 2) + (4 * (a ** 2) * (self.z_si ** 2))))
-                )
-            )
-            v_t = (-1 / np.sqrt(1 - np.square(self.z_si / r))) * (
-                (self.vz_si * r - v_r * self.z_si) / (r ** 2)
-            )
-            v_p = (1 / (1 + np.square(self.y_si / self.x_si))) * (
-                (self.vy_si * self.x_si - self.vx_si * self.y_si) / (self.x_si ** 2)
-            )
-
-            return r, theta, phi, v_r, v_t, v_p, a
-        return r, theta, phi, a
+        return cartesian_to_bl_fast(
+            self.x_si,
+            self.y_si,
+            self.z_si,
+            a,
+            self.vx_si,
+            self.vy_si,
+            self.vz_si,
+            self._velocities_provided,
+        )
 
 
 class SphericalConversion:
@@ -82,26 +67,15 @@ class SphericalConversion:
         return self.r_si, self.t_si, self.p_si
 
     def convert_cartesian(self):
-        x = self.r_si * np.cos(self.p_si) * np.sin(self.t_si)
-        y = self.r_si * np.sin(self.p_si) * np.sin(self.t_si)
-        z = self.r_si * np.cos(self.t_si)
-        if self._velocities_provided:
-            v_x = (
-                np.sin(self.t_si) * np.cos(self.p_si) * self.vr_si
-                - self.r_si * np.sin(self.t_si) * np.sin(self.p_si) * self.vp_si
-                + self.r_si * np.cos(self.t_si) * np.cos(self.p_si) * self.vt_si
-            )
-            v_y = (
-                np.sin(self.t_si) * np.sin(self.p_si) * self.vr_si
-                + self.r_si * np.cos(self.t_si) * np.sin(self.p_si) * self.vt_si
-                + self.r_si * np.sin(self.t_si) * np.cos(self.p_si) * self.vp_si
-            )
-            v_z = (
-                np.cos(self.t_si) * self.vr_si
-                - self.r_si * np.sin(self.t_si) * self.vt_si
-            )
-            return x, y, z, v_x, v_y, v_z
-        return x, y, z
+        return spherical_to_cartesian_fast(
+            self.r_si,
+            self.t_si,
+            self.p_si,
+            self.vr_si,
+            self.vt_si,
+            self.vp_si,
+            self._velocities_provided,
+        )
 
     def convert_bl(self, a):
         transformed_cartesian = self.convert_cartesian()
@@ -136,27 +110,16 @@ class BoyerLindquistConversion:
         return self.r_si, self.t_si, self.p_si, self.a_si
 
     def convert_cartesian(self):
-        xa = np.sqrt(self.r_si ** 2 + self.a_si ** 2)
-        sin_norm = xa * np.sin(self.t_si)
-        x = sin_norm * np.cos(self.p_si)
-        y = sin_norm * np.sin(self.p_si)
-        z = self.r_si * np.cos(self.t_si)
-        if self._velocities_provided:
-            v_x = (
-                (self.r_si * self.vr_si * np.sin(self.t_si) * np.cos(self.p_si) / xa)
-                + (xa * np.cos(self.t_si) * np.cos(self.p_si) * self.vt_si)
-                - (xa * np.sin(self.t_si) * np.sin(self.p_si) * self.vp_si)
-            )
-            v_y = (
-                (self.r_si * self.vr_si * np.sin(self.t_si) * np.sin(self.p_si) / xa)
-                + (xa * np.cos(self.t_si) * np.sin(self.p_si) * self.vt_si)
-                + (xa * np.sin(self.t_si) * np.cos(self.p_si) * self.vp_si)
-            )
-            v_z = (self.vr_si * np.cos(self.t_si)) - (
-                self.r_si * np.sin(self.t_si) * self.vt_si
-            )
-            return x, y, z, v_x, v_y, v_z
-        return x, y, z
+        return bl_to_cartesian_fast(
+            self.r_si,
+            self.t_si,
+            self.p_si,
+            self.a_si,
+            self.vr_si,
+            self.vt_si,
+            self.vp_si,
+            self._velocities_provided,
+        )
 
     def convert_spherical(self):
         transformed_cartesian = self.convert_cartesian()
