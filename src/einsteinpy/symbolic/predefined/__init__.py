918c1dfb26a276c5a015f86f74f09cfda5db3a1d
Ritwik Saha <s.ritwik98@gmail.com>
2020-01-17 14:56:10 +0530

    two new predefined metrics

--- src/einsteinpy/symbolic/predefined/__init__.py
+++ src/einsteinpy/symbolic/predefined/__init__.py
@@ -1,4 +1,6 @@
 from .cmetric import CMetric
+from .davidson import Davidson
 from .de_sitter import AntiDeSitter, AntiDeSitterStatic, DeSitter
+from .godel import Godel
 from .minkowski import Minkowski
 from .vacuum_solutions import Schwarzschild
