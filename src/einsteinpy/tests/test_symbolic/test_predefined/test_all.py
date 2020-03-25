a776f7c887cd27995c8dca0e633fb49cff933ec9
Shreyas Kalvankar <42400733+obi-wan-shinobi@users.noreply.github.com>
2020-03-25 21:23:50 +0530

    Update test_all.py

--- src/einsteinpy/tests/test_symbolic/test_predefined/test_all.py
+++ src/einsteinpy/tests/test_symbolic/test_predefined/test_all.py
@@ -75,12 +75,13 @@ def test_check_two_metrics_are_equal(m1, m2):
     ],
 )
 def test_check_ReissnerNordstorm(m1, m2):
-    test_arr = Array(np.zeros(shape=m1.tensor().shape, dtype=int))
+    test_arr = np.zeros(shape=m1.tensor().shape, dtype=int)
     coords = symbols("t r theta phi")
     t, r, theta, phi = coords
     c, G, eps_0, Q = constants.c, constants.G, constants.eps_0, symbols("Q")
     rQsq = ((Q ** 2) * G) / (4 * pi * eps_0 * (c ** 4))
     test_arr[0][0] = rQsq / r ** 2
+    test_arr = Array(test_arr)
     assert simplify_sympy_array(m1.tensor() - m2.tensor()) == test_arr
 
 
