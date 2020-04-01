38f8c09d41908c22407fc90f8c508803c91bbd4c
ali1810 <49233294+ali1810@users.noreply.github.com>
2020-04-01 16:00:06 +0200

    little change in doc

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 
 # https://packaging.python.org/guides/single-sourcing-package-version/
 # http://blog.ionelmc.ro/2014/05/25/python-packaging/
-
+# Good to start 
 setup(setup_cfg=True)
