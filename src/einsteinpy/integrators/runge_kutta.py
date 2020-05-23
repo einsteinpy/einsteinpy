fc0ed0ad32ee1db79edddc57648532e678356371
Jyotirmaya Shivottam <31299799+JeS24@users.noreply.github.com>
2020-05-23 12:40:02 +0530

    Draft PR - integrator | metric

--- src/einsteinpy/integrators/runge_kutta.py
+++ src/einsteinpy/integrators/runge_kutta.py
@@ -112,3 +112,94 @@ class RK45(integrate.RK45):
                 "Attempt to step on a failed or finished solver. (Invalid Value or out of bounds of t_bound)",
                 RuntimeWarning,
             )
+
+
+# DRAFT CHANGES/ADDITIONS -- STARTS HERE - ????
+# Cannot make adaptive meshing an optional parameter 
+# in the RK45 class, above, as stepsize control is
+# not available in scipy.integrate.
+# In prep for use with KerrShadow in rays.shadow
+class RK45Adaptive(): #
+    """
+    Implements RK45 integration scheme, with Adaptive Stepsize Control
+    """
+
+    def __init__(self, fun, t0, y0, t_bound, stepsize, rtol=None, atol=None):
+        """
+        Initialization
+
+        Parameters
+        ----------
+        fun : function
+            Should accept t, y as parameters, and return same type as y
+        t0 : float
+            Initial t (Affine parameter, Proper Time for timelike geodesics) - ????
+        y0 : ~numpy.array or float
+            Initial State Vector - ????
+        t_bound : float
+            Integration Bounds - the integration won’t continue beyond it. It also determines the direction of the integration.
+        stepsize : float
+            Size of initial step - ????
+        
+        # Default values have to chosen balancing accuracy and performance - ????
+        rtol : float
+            Relative tolerance, defaults to 0.2*stepsize
+        atol : float
+            Absolute tolerance, defaults to rtol/0.8e3
+        
+        """
+        pass
+
+    def _nextstep(x1, x2, x3, u1, u2, u3, prev_step):
+        """
+        Returns next stepsize, which depends on position (x), velocity (u) and previous stepsize
+        Specific to BL Coordinates, for now - ????
+
+        Based on RAPTOR (2018) - ????
+
+        Parameters
+        ----------
+        x1 : 
+            Value of X^1 Coordinate (r, in BL Coordinates)
+        x2 : 
+            Value of X^2 Coordinate (theta, in BL Coordinates)
+        x3 : 
+            Value of X^3 Coordinate (phi, in BL Coordinates)
+        u1 : 
+            Value of U^1 Coordinate (u_r, in BL Coordinates)
+        u2 : 
+            Value of U^2 Coordinate (u_theta, in BL Coordinates)
+        u3 : 
+            Value of U^3 Coordinate (u_phi, in BL Coordinates)
+        prev_step : float
+            Size of previous integration step
+        
+        Returns
+        -------
+        float :
+            Stepsize for next integration step
+            
+        """
+        # To protect against division by 0
+        delta =  1e-20 # ???? Choose appropriate delta
+        delta2 = delta**2
+
+        dlx1  = prev_step / (np.abs(u1) + delta2)
+        dlx2  = prev_step * np.min(x2, 1. - x2)/ (np.abs(u2) + delta2)
+        dlx3  = prev_step / (np.abs(u3) + delta2)
+
+        idlx1 = 1. / (np.abs(dlx1) + delta2)
+        idlx2 = 1. / (np.abs(dlx2) + delta2)
+        idlx3 = 1. / (np.abs(dlx3) + delta2)
+
+        next_step = -np.max([1. / (idlx1 + idlx2 + idlx3), 1e-12])
+        return next_step
+
+    def step(self):
+        """
+        Moves integration forward by one step
+        """
+        # Calls _nextstep() for getting the stepsize
+        pass
+
+# DRAFT CHANGES/ADDITIONS -- ENDS HERE - ????
