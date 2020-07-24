b38e222c17219522fa8fe7763551335e2856047f
Jyotirmaya Shivottam <31299799+JeS24@users.noreply.github.com>
2020-07-24 18:25:14 +0530

    NullGeod - initial implementation

--- src/einsteinpy/geodesic/nulllike.py
+++ src/einsteinpy/geodesic/nulllike.py
@@ -1,33 +1,38 @@
-# Issues:
-# 1. Sign of f_vec
-# 2. NullBundle
-
-
 import warnings
 
 import numpy as np
 
-from einsteinpy.geodesic import Geodesic
-from einsteinpy.geodesic.utils import v1  # - ????
+from einsteinpy.coordinates.utils import bl_to_cartesian_fast
+from einsteinpy.geodesic.utils import _calc_state, _christoffels_M, _f_geod_M
 from einsteinpy.integrators import RK45
 
 
-class NullGeodesic(Geodesic):
+class NullGeodesic:
     """
     Class for defining Null-like Geodesics
 
     """
 
+    # - ????? Too many parameters -
+    # Can remove E
+    # Consolidate alpha, beta, rcam & inclination
+    # Consolidate end_lambda, step_size, return_cartesian as ODEKwargs
     def __init__(
-        self, metric, coords, end_lambda, step_size=1e-3, return_cartesian=True
+        self,
+        alpha,
+        beta,
+        rcam=1e4,
+        inclination=np.pi / 2,
+        a=0.0,
+        E=1.0,
+        end_lambda=10.0,
+        step_size=1e-3,
+        return_cartesian=True,
     ):
         """
         Parameters
         ----------
-        metric : ~einsteinpy.metric.*
-            Metric, in which Geodesics are to be calculated
-        coords : ~einsteinpy.coordinates.core.*
-            Coordinate system, in which Metric is to be represented
+        # - Docstring ?????
         end_lambda : float
             Affine Parameter, Lambda, where iterations will stop
         step_size : float, optional
@@ -38,66 +43,211 @@ class NullGeodesic(Geodesic):
             Defaults to ``True``
 
         """
-        super().__init__(
-            time_like=False,
-            metric=metric,
-            coords=coords,
-            end_lambda=end_lambda,
-            step_size=step_size,
-            return_cartesian=return_cartesian,
-        )
+        self.alpha = alpha
+        self.beta = beta
+        self.rcam = rcam
+        self.inclination = inclination
+        self.a = a
+        self.E = E  # - ?????
 
+        # self._sch_rad = 2
         self._state = self._calculate_state()
-        # Trajectory integration will be done
-        # by calculate_trajectory() in Geodesic
+        self._trajectory = self.calculate_trajectory(
+            end_lambda=end_lambda,
+            OdeMethodKwargs={"stepsize": step_size},
+            return_cartesian=return_cartesian,
+        )[1]
+
+    def __repr__(self):
+        return f"NullGeodesic Object:\n\
+            Initial Conditions = ({self.alpha, self.beta, self.rcam, self.inclination}),\n\
+            Initial State = ({self.state}),\n\
+            Trajectory = ({self.trajectory})"
+
+    def __str__(self):
+        return f"NullGeodesic Object:\n\
+            Initial Conditions = ({self.alpha, self.beta, self.rcam, self.inclination}),\n\
+            Initial State = ({self.state}),\n\
+            Trajectory = ({self.trajectory})"
+
+    @property
+    def state(self):
+        """
+        Returns the Initial State Vector of the Geodesic
+
+        """
+        return self._state
+
+    @property
+    def trajectory(self):
+        """
+        Returns the "Trajectory" of the Geodesic
+
+        """
+        return self._trajectory
 
     def _calculate_state(self):
         """
         Prepares and returns the Initial State Vector of the massless test particle
 
-        # Returns
-        # -------
-        # state : ~numpy.ndarray
-        #     Initial State Vector of the massless test particle
-        #     Length-8 Array
+        Source: RAPTOR (?????)
 
-        # Raises
-        # ------
-        # TypeError
-        #     If there is a mismatch between the coordinates class of ``self.coords`` and \
-        #     coordinate class, with which ``self.metric`` object has been instantiated
+        Returns
+        -------
+        state : ~numpy.ndarray
+            Initial State Vector of the massless test particle
+            Length-8 Array
 
         """
-        if self.coords.system != self.metric.coords.system:
-            raise TypeError(
-                "Coordinate System Mismatch between Metric object and supplied initial coordinates."
-            )
+        a = self.a
+        E = self.E
+        r = self.rcam
+        alpha, beta = self.alpha, self.beta
+        theta = self.inclination
 
-        x4 = self.coords.position()
-        al = x4[1]
-        be = self.metric.sch_rad * 5
-        i = x4[2]
+        return _calc_state(alpha, beta, r, theta, a, E)
 
-        g_cov_mat = self.metric.metric_covariant(x4)
+    # Move to Kerr - ?????
+    # Not until units + coordinate switching is figured out
+    def _ch_sym_M(self, x_vec):
+        """
+        Returns Christoffel Symbols for Kerr Metric \
+        in Boyer-Lindquist Coordinates, in M-Units
 
-        # CHECK UNITS - ?????
-        E = 1.0
+        Parameters
+        ----------
+        x_vec : array_like
+            Position 4-Vector
 
-        L = -al * E * np.sqrt(1 - np.cos(i) ** 2)
-        Q = (E ** 2) * (be ** 2 + (np.cos(i) ** 2) * (al ** 2 - 1))
+        Returns
+        -------
+        ~numpy.ndarray
+            Christoffel Symbols for Kerr Metric \
+            in Boyer-Lindquist Coordinates \
+            in M-Units
+            Numpy array of shape (4,4,4)
 
-        k_t = -E
-        k_p = L
-        k_th = np.sign(be) * np.sqrt(
-            np.abs(Q - (L * np.cot(i)) ** 2 + (E * np.cos(i)) ** 2)
+        """
+        r, th = x_vec[1], x_vec[2]
+
+        return _christoffels_M(self.a, r, th)
+
+    # Move to Kerr - ?????
+    # Not until units + coordinate switching is figured out
+    def _f_vec_M(self, lambda_, vec):
+        """
+        Returns f_vec for Kerr Metric \
+        in Boyer-Lindquist Coordinates \
+        in M-Units (G = c = M = 1)
+
+        To be used for solving Geodesics ODE
+
+        Source: RAPTOR (?????)
+
+        Parameters
+        ----------
+        lambda_ : float
+            Parameterizes current integration step
+            Used by ODE Solver
+
+        vec : array_like
+            Length-8 Vector, containing 4-Position & 4-Velocity
+
+        Returns
+        -------
+        ~numpy.ndarray
+            f_vec for Kerr Metric in Boyer-Lindquist Coordinates
+            Numpy array of shape (8)
+
+        """
+        chl = self._ch_sym_M(vec[:4])
+        vals = np.zeros(shape=vec.shape, dtype=vec.dtype)
+
+        return _f_geod_M(chl, vals, vec)
+
+    def calculate_trajectory(
+        self,
+        end_lambda=10.0,
+        OdeMethodKwargs={"stepsize": 1e-3},
+        return_cartesian=True,
+    ):
+        """
+        Calculate trajectory in spacetime, according to Geodesic Equations
+
+        Parameters
+        ----------
+        end_lambda : float, optional
+            Affine Parameter, Lambda, where iterations will stop
+            Equivalent to Proper Time for Timelike Geodesics
+            Defaults to ``10.0``
+        OdeMethodKwargs : dict, optional
+            Kwargs to be supplied to the ODESolver
+            Dictionary with key 'stepsize' along with a float value is expected
+            Defaults to ``{'stepsize': 1e-3}``
+        return_cartesian : bool, optional
+            Whether to return calculated values in Cartesian Coordinates
+            Defaults to ``True``
+
+        Returns
+        -------
+        ~numpy.ndarray
+            N-element numpy array containing Lambda, where the geodesic equations were evaluated
+        ~numpy.ndarray
+            (n,8) shape numpy array containing [x0, x1, x2, x3, v0, v1, v2, v3] for each Lambda
+
+        """
+        ODE = RK45(
+            fun=self._f_vec_M,
+            t0=0.0,
+            y0=self.state,
+            t_bound=end_lambda,
+            **OdeMethodKwargs,
         )
-        k_r = v1(g_cov_mat, k_t, k_th, k_p)
 
-        v4 = [k_t, k_p, k_th, k_r]
+        a = self.a
+        r = self.rcam
 
-        state = np.hstack((x4, v4))
+        # Termination conditions
+        cutoff_outer = r * 1.01
+        cutoff_inner = (1.0 + np.sqrt(1.0 - a ** 2)) * 1.1
 
-        return state
+        vecs = list()
+        lambdas = list()
+
+        while ODE.t < end_lambda:
+            vecs.append(ODE.y)
+            lambdas.append(ODE.t)
+
+            r_curr = ODE.y[1]
+
+            # Checking termination conditions
+            if r_curr < cutoff_inner or r_curr > cutoff_outer:
+                warnings.warn("Light Ray has reached cut-off bounds.", RuntimeWarning)
+                break
+
+            ODE.step()
+
+        vecs, lambdas = np.array(vecs), np.array(lambdas)
+
+        if return_cartesian:
+            cart_vecs = list()
+            for v in vecs:
+                vals = bl_to_cartesian_fast(
+                    v[0],
+                    v[1],
+                    v[2],
+                    v[3],
+                    a,
+                    v[5],
+                    v[6],
+                    v[7],
+                    velocities_provided=True,
+                )
+                cart_vecs.append(np.hstack((vals[:4], v[4], vals[4:])))
+
+            return lambdas, np.array(cart_vecs)
+
+        return lambdas, vecs
 
 
 class NullBundle:
--- src/einsteinpy/geodesic/utils.py
+++ src/einsteinpy/geodesic/utils.py
@@ -1,40 +1,144 @@
-def v1(g_cov_mat, v0, v2, v3):
+import numpy as np
+
+from einsteinpy.ijit import jit
+
+
+# @jit(parallel=True)
+def _calc_state(alpha, beta, r, theta, a, E):
     """
-        OUTDATED - ?????
-        Utility function to return first space-like of 4-Velocity
-        Assumes a (+, -, -, -) Metric Signature
+    Jit-ed funtion for calculating state
 
-        Parameters
-        ----------
-        g_cov_mat : ~numpy.ndarray
-            Matrix, containing Covariant Metric \
-            Tensor values, in same coordinates as ``v_vec``
-            Numpy array of shape (4,4)
-        v0 : float
-            First component of 4-Velocity
-        v2 : float
-            Third component of 4-Velocity
-        v3 : float
-            Fourth component of 4-Velocity
+    Source: RAPTOR (?????)
 
-        Returns
-        -------
-        float
-            First spacelike component of 4-Velocity/4-Momentum
+    """
+    # Useful expressions
+    sint, cost, tant = np.sin(theta), np.cos(theta), np.tan(theta)
+    sint2, cost2 = sint ** 2, cost ** 2
+    r2, a2 = r ** 2, a ** 2
+    sg = r2 + a2 * cost2
+    dl = r2 + a2 - 2.0 * r
 
-        """
-    g = g_cov_mat
-    # fac = 0, so removed from C
-    # Defining coefficients for quadratic equation
-    A = g[1, 1]
-    B = 2 * (g[0, 1] * v0 + g[1, 2] * v2 + g[1, 3] * v3)
-    C = (
-        (g[0, 0] * v0 ** 2 + g[2, 2] * v2 ** 2 + g[3, 3] * v3 ** 2)
-        + 2 * v0 * (g[0, 2] * v2 + g[0, 3] * v3)
-        + 2 * v2 * g[2, 3] * v3
+    # Constants of Motion
+    L = -alpha * E * sint
+    Q = (E ** 2) * (beta ** 2 + cost2 * (alpha ** 2 - 1.0))
+
+    # Co- & Contra-variant wave vectors
+    k_d = np.zeros(4)
+    k_u = np.zeros(4)
+
+    k_d[0] = -E
+    k_d[2] = np.sign(beta) * np.sqrt(np.abs(Q - (L / tant) ** 2 + cost2 * (E ** 2)))
+    k_d[3] = L
+
+    # Relevant metric components, required to build k_u
+    g_dd_11 = sg / dl
+    g_uu_00 = -(((r2 + a2) ** 2) - (dl * a2 * sint2)) / (sg * dl)
+    g_uu_03 = -2.0 * a * r / (sg * dl)
+    g_uu_33 = (dl - a2 * sint2) / (sg * dl * sint2)
+    g_uu_22 = 1.0 / sg
+
+    # Building k_u
+    k_u[0] = g_uu_00 * k_d[0] + g_uu_03 * k_d[3]
+    k_u[3] = g_uu_33 * k_d[3] + g_uu_03 * k_d[0]
+    k_u[2] = g_uu_22 * k_d[2]
+    k_u[1] = np.sqrt((-k_u[0] * k_d[0] - k_u[2] * k_d[2] - k_u[3] * k_d[3]) / g_dd_11)
+
+    # Constructing State Vector
+    x_u = np.array([0.0, r, theta, np.pi / 2])  # Position Vector
+
+    state = np.hstack((x_u, k_u))
+
+    return state
+
+
+# @jit(parallel=True)
+def _christoffels_M(a, r, th):
+    """
+    Jit-ed funtion for calculating Christoffel Symbols \
+    in Kerr Metric in M-Units & Boyer-Lindquist Coordinates
+
+    Source: RAPTOR (?????)
+
+    """
+    # Useful expressions
+    sint, cost = np.sin(th), np.cos(th)
+    sint2, cost2 = sint ** 2, cost ** 2
+    r2, a2 = r ** 2, a ** 2
+    sg = r2 + a2 * cost2
+    dl = r2 + a2 - 2 * r
+    sg2, sg3 = sg ** 2, sg ** 3
+
+    chl = np.zeros((4, 4, 4))
+
+    # Non-zero components
+    chl[0, 0, 1] = (r2 + a2) / (sg2 * dl) * (2.0 * r2 - sg)
+    chl[0, 0, 2] = -2.0 * a2 * r * sint * cost / sg2
+    chl[0, 1, 3] = -a * sint2 / (sg * dl) * (2.0 * r2 / sg * (r2 + a2) + r2 - a2)
+    chl[0, 2, 3] = 2.0 * a2 * a * r * sint2 * sint * cost / sg2
+    chl[1, 0, 0] = dl / sg3 * (2.0 * r2 - sg)
+    chl[1, 0, 3] = -a * dl * sint2 / sg3 * (2.0 * r2 - sg)
+    chl[1, 1, 1] = (1.0 - r) / dl + r / sg
+    chl[1, 1, 2] = -a2 * sint * cost / sg
+    chl[1, 2, 2] = -r * dl / sg
+    chl[1, 3, 3] = -dl * sint2 / sg * (r - a2 * sint2 / sg2 * (2.0 * r2 - sg))
+    chl[2, 0, 0] = -2.0 * a2 * r * sint * cost / sg3
+    chl[2, 0, 3] = 2.0 * a * r * (r2 + a2) * sint * cost / sg3
+    chl[2, 1, 1] = a2 * sint * cost / (sg * dl)
+    chl[2, 1, 2] = r / sg
+    chl[2, 3, 3] = (
+        -sint
+        * cost
+        / sg3
+        * ((r2 + a2) * (((r2 + a2) ** 2) - dl * a2 * sint2) - sg * dl * a2 * sint2)
     )
-    D = (B ** 2) - (4 * A * C)
+    chl[3, 0, 1] = a / (sg2 * dl) * (2.0 * r2 - sg)
+    chl[3, 0, 2] = -2.0 * a * r * cost / (sg2 * sint)
+    chl[3, 1, 3] = r / sg - a2 * sint2 / (sg * dl) * (r - 1.0 + 2.0 * r2 / sg)
+    chl[3, 2, 3] = cost / sint * (1.0 + 2.0 * a2 * r * sint2 / sg2)
+    chl[2, 2, 2] = chl[1, 1, 2]
 
-    v1 = (-B + np.sqrt(D)) / (2 * A)
+    return chl
 
-    return v1
+
+# @jit(parallel=True)
+def _f_geod_M(chl, vals, vec):
+    """
+    Jit-ed funtion for calculating ``f_vec`` in \
+    Kerr Metric in M-Units & Boyer-Lindquist Coordinates
+
+    Source: RAPTOR (?????)
+
+    """
+
+    vals[:4] = vec[4:]
+
+    vals[4] = -2.0 * (
+        chl[0, 0, 1] * vec[4] * vec[5]
+        + chl[0, 0, 2] * vec[4] * vec[6]
+        + chl[0, 1, 3] * vec[5] * vec[7]
+        + chl[0, 2, 3] * vec[6] * vec[7]
+    )
+    vals[5] = -1.0 * (
+        chl[1, 0, 0] * vec[4] * vec[4]
+        + 2 * chl[1, 0, 3] * vec[4] * vec[7]
+        + chl[1, 1, 1] * vec[5] * vec[5]
+        + 2 * chl[1, 1, 2] * vec[5] * vec[6]
+        + chl[1, 2, 2] * vec[6] * vec[6]
+        + chl[1, 3, 3] * vec[7] * vec[7]
+    )
+    vals[6] = -1.0 * (
+        chl[2, 0, 0] * vec[4] * vec[4]
+        + 2 * chl[2, 0, 3] * vec[4] * vec[7]
+        + chl[2, 1, 1] * vec[5] * vec[5]
+        + 2 * chl[2, 1, 2] * vec[5] * vec[6]
+        + chl[2, 2, 2] * vec[6] * vec[6]
+        + chl[2, 3, 3] * vec[7] * vec[7]
+    )
+    vals[7] = -2.0 * (
+        chl[3, 0, 1] * vec[4] * vec[5]
+        + chl[3, 0, 2] * vec[4] * vec[6]
+        + chl[3, 1, 3] * vec[5] * vec[7]
+        + chl[3, 2, 3] * vec[6] * vec[7]
+    )
+
+    return vals
