250b438db60860788ada0070330d32a87fc9b6bb
Qbiwan <Qbiwan1934@yahoo.com>
2021-01-04 14:01:04 +0800

    isort __init__.py

--- src/einsteinpy/symbolic/__init__.py
+++ src/einsteinpy/symbolic/__init__.py
@@ -3,14 +3,6 @@ from .constants import SymbolicConstant, get_constant
 from .einstein import EinsteinTensor
 from .helpers import TransformationMatrix, simplify_sympy_array
 from .metric import MetricTensor
-from .ricci import RicciScalar, RicciTensor
-from .riemann import RiemannCurvatureTensor
-from .schouten import SchoutenTensor
-from .stress_energy_momentum import StressEnergyMomentumTensor
-from .tensor import BaseRelativityTensor, Tensor
-from .vector import GenericVector
-from .weyl import WeylTensor
-
 from .predefined.alcubierre_warp import AlcubierreWarp
 from .predefined.barriola_vilenkin import BarriolaVilekin
 from .predefined.bertotti_kasner import BertottiKasner
@@ -23,8 +15,19 @@ from .predefined.find import find
 from .predefined.godel import Godel
 from .predefined.janis_newman_winicour import JanisNewmanWinicour
 from .predefined.minkowski import Minkowski, MinkowskiCartesian, MinkowskiPolar
-from .predefined.vacuum_solutions import Kerr, KerrNewman, ReissnerNordstorm, Schwarzschild
-
+from .predefined.vacuum_solutions import (
+    Kerr,
+    KerrNewman,
+    ReissnerNordstorm,
+    Schwarzschild,
+)
+from .ricci import RicciScalar, RicciTensor
+from .riemann import RiemannCurvatureTensor
+from .schouten import SchoutenTensor
+from .stress_energy_momentum import StressEnergyMomentumTensor
+from .tensor import BaseRelativityTensor, Tensor
+from .vector import GenericVector
+from .weyl import WeylTensor
 
 __all__ = ["ChristoffelSymbols",
            "SymbolicConstant",
