4d210282314daacba79ebca273cf96a23c1fe75e
Qbiwan <Qbiwan1934@yahoo.com>
2021-01-06 21:59:02 +0800

    added one blank line from pep8 warning

--- tests/test_ijit/test_ijit.py
+++ tests/test_ijit/test_ijit.py
@@ -3,6 +3,7 @@ import numba
 
 from einsteinpy.ijit import ijit
 
+
 @pytest.mark.parametrize("test_input, expected", [(None, "_jit"), (numba.njit, "njit")])
 def test_correct_func_returned(test_input, expected):
     returned_func = ijit(test_input)
