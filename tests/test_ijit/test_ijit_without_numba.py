8481f13a020a2dd2c4b8f3036a832875990ef388
Qbiwan <Qbiwan1934@yahoo.com>
2021-01-09 17:00:06 +0800

    mock tests added

--- tests/test_ijit/test_ijit_without_numba.py
+++ tests/test_ijit/test_ijit_without_numba.py
@@ -8,7 +8,7 @@ import einsteinpy.ijit
 
 
 @mock.patch.dict(sys.modules, {"numba": None})
-def test():
+def test_decorator():
     with warnings.catch_warnings(record=True) as w:
         custom_jit = reload(einsteinpy.ijit)
 
@@ -19,5 +19,25 @@ def test():
         assert len(w) >= 1
         assert isinstance(_simple_func, types.FunctionType)
 
+@mock.patch.dict(sys.modules, {"numba": None})
+def test_function_given():
+    with warnings.catch_warnings(record=True) as w:
+        custom_jit = reload(einsteinpy.ijit)
 
-custom_jit = reload(einsteinpy.ijit)
+        def _simple_func(a, b):
+            return a + b
+
+        return_func = custom_jit.jit(_simple_func)
+
+        assert len(w) >= 1
+        assert return_func.__name__ == "_simple_func"
+
+
+@mock.patch.dict(sys.modules, {"numba": None})
+def test_no_function_given():
+    with warnings.catch_warnings(record=True) as w:
+        custom_jit = reload(einsteinpy.ijit)
+        returned_func = custom_jit.jit()
+
+        assert len(w) >= 1
+        assert returned_func.__name__ == "_jit"
