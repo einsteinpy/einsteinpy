9df997e157900e806b03da55fd069d0a0d3cc648
Qbiwan <Qbiwan1934@yahoo.com>
2021-01-09 19:01:37 +0800

    combined 2 tests to one and got rid of warnings

--- tests/test_ijit/test_ijit_without_numba.py
+++ tests/test_ijit/test_ijit_without_numba.py
@@ -8,7 +8,7 @@ import einsteinpy.ijit
 
 
 @mock.patch.dict(sys.modules, {"numba": None})
-def test():
+def test_warning_and_returntype():
     with warnings.catch_warnings(record=True) as w:
         custom_jit = reload(einsteinpy.ijit)
 
@@ -19,5 +19,14 @@ def test():
         assert len(w) >= 1
         assert isinstance(_simple_func, types.FunctionType)
 
+@mock.patch.dict(sys.modules, {"numba": None})
+def test_decorator():
+    custom_jit = reload(einsteinpy.ijit)
 
-custom_jit = reload(einsteinpy.ijit)
+    def _simple_func(a, b):
+        return a + b
+
+    res = custom_jit.jit()
+
+    assert res.__name__ == "_jit"
+    assert res(_simple_func).__name__ == "_simple_func"
