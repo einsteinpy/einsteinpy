1630c3566b25c32f7750c7372321d634fda0d628
aweinr4 <78507140+aweinr4@users.noreply.github.com>
2021-02-16 11:47:53 -0500

    Update riemann.py
    updated docstring

--- src/einsteinpy/symbolic/riemann.py
+++ src/einsteinpy/symbolic/riemann.py
@@ -55,7 +55,9 @@ class RiemannCurvatureTensor(BaseRelativityTensor):
     @classmethod
     def from_christoffels(cls, chris, parent_metric=None):
         """
-        Get Riemann Tensor calculated from a Christoffel Symbols
+        Get Riemann Tensor calculated from a Christoffel Symbols Reimann Tensor is given as:
+        .. math::
+           R^{t}{}_{s r n}=\Gamma^{t}{}_{s n, r} - \Gamma^{t }{}_{s r, n } + \Gamma^{p}{}_{s n}\Gamma^{t}{}_{p r} - \Gamma^{p}{}_{s r}\Gamma^{t}{}_{p n}
 
         Parameters
         ----------
