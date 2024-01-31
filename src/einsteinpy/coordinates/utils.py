01e5018f485dff906507962e469a5d1fc4d0fabb
Tony Gyan <49129216+TONYOG12@users.noreply.github.com>
2024-01-31 16:25:19 +0000

    Update utils.py

--- src/einsteinpy/coordinates/utils.py
+++ src/einsteinpy/coordinates/utils.py
@@ -1,7 +1,4 @@
-from typing import Tuple
-
 import numpy as np
-from numba import float64, jit
 
 from einsteinpy import constant
 from einsteinpy.ijit import jit
@@ -9,9 +6,8 @@ from einsteinpy.ijit import jit
 _c = constant.c.value
 
 
-def calculate_coordinates(
-    r: float64, th: float64, p: float64, alpha: float64
-) -> Tuple[float64, float64, float64, float64]:
+@jit
+def calculate_coordinates(r, th, p, alpha):
     xa = np.sqrt(r**2 + alpha**2)
     sin_norm = xa * np.sin(th)
     e0 = sin_norm * np.cos(p)
